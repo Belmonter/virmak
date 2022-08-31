f2efc375413ffa27771901cc9fc50fdc6b2027fa
Oleg <GabrialBWeb@yandex.ru>
2022-08-31 20:18:53 +0300

    add descktop and burger menu

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -1,19 +1,5 @@
-/*
-Документация по работе в шаблоне: 
-Документация слайдера: https://swiperjs.com/
-Сниппет(HTML): swiper
-*/
 
-// Подключаем слайдер Swiper из node_modules
-// При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
-// Пример: { Navigation, Autoplay }
 import Swiper, { Navigation, Grid } from 'swiper';
-/*
-Основные модули слайдера:
-Navigation, Pagination, Autoplay, 
-EffectFade, Lazy, Manipulation
-Подробнее смотри https://swiperjs.com/
-*/
 
 // Стили Swiper
 // Базовые стили
@@ -23,17 +9,9 @@ import "../../scss/libs/swiper.scss";
 // Полный набор стилей из node_modules
 // import 'swiper/css';
 
-// Инициализация слайдеров
 function initSliders() {
-	// Перечень слайдеров
-	// Проверяем, есть ли слайдер на странице
 	if (document.querySelector('.projects__slider')) {
-		// Указываем класс нужного слайдера
-		// Создаем слайдер
 		new Swiper('.projects__slider', {
-			// Указываем класс нужного слайдера
-			// Подключаем модули слайдера
-			// для конкретного случая
 			modules: [Navigation, Grid],
 			observer: true,
 			observeParents: true,
@@ -48,45 +26,6 @@ function initSliders() {
 				nextEl: '.project-slide-right',
 				prevEl: '.project-slide-left',
 			},
-
-			//touchRatio: 0,
-			//simulateTouch: false,
-			//loop: true,
-			//preloadImages: false,
-			//lazy: true,
-
-			/*
-			// Эффекты
-			effect: 'fade',
-			autoplay: {
-				delay: 3000,
-				disableOnInteraction: false,
-			},
-			*/
-
-			// Пагинация
-			/*
-			pagination: {
-				el: '.swiper-pagination',
-				clickable: true,
-			},
-			*/
-
-			// Скроллбар
-			/*
-			scrollbar: {
-				el: '.swiper-scrollbar',
-				draggable: true,
-			},
-			*/
-
-			// Кнопки "влево/вправо"
-			// navigation: {
-			// 	prevEl: '.swiper-button-prev',
-			// 	nextEl: '.swiper-button-next',
-			// },
-
-			// Брейкпоинты
 			/*
 			breakpoints: {
 				320: {
@@ -109,6 +48,89 @@ function initSliders() {
 			},
 			*/
 			// События
+			on: {},
+		});
+	}
+
+  if (document.querySelector('.credit__slider')) {
+		new Swiper('.credit__slider', {
+			modules: [Navigation],
+			observer: true,
+			observeParents: true,
+			spaceBetween: 20,
+			slidesPerView: 3,
+			centerInsufficientSlides: true,
+			navigation: {
+				nextEl: '.credit__slide-right',
+				prevEl: '.credit__slide-left',
+			},
+			// navigation: {
+			// 	prevEl: '.swiper-button-prev',
+			// 	nextEl: '.swiper-button-next',
+			// },
+			/*
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+			on: {},
+		});
+	}
+
+  if (document.querySelector('.video__slider')) {
+		new Swiper('.video__slider', {
+			modules: [Navigation],
+			observer: true,
+			observeParents: true,
+			spaceBetween: 20,
+      watchOverflow: true,
+			slidesPerView: 3,
+			centerInsufficientSlides: true,
+			navigation: {
+				nextEl: '.video-slider-arrow-right',
+				prevEl: '.video-slider-arrow-left',
+			},
+			// navigation: {
+			// 	prevEl: '.swiper-button-prev',
+			// 	nextEl: '.swiper-button-next',
+			// },
+			/*
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
 			on: {},
 		});
 	}
